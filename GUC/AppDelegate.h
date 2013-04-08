2c7e1ab1463cc77f40717452a145d6590a16b667
Michael Brodeur <mikejoebro@localhost.com>
2013-04-08 11:01:13 -0400

    Initial Commit

---
+++ GUC/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  GUC
+//
+//  Created by Michael Brodeur on 4/8/13.
+//  Copyright (c) 2013 SmashingBoxes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ GUC/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GUC
+//
+//  Created by Michael Brodeur on 4/8/13.
+//  Copyright (c) 2013 SmashingBoxes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
